5c11acf9e2d450f7b7c0e9d320c4533255db5ee2
parks <pshauukn@hansung.ac.kr>
2024-04-11 19:45:02 +0900

    프로젝트 경로 정리 (2) main 함수 바깥으로 뺌

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import '../home.dart';
-import 'home_page/my_home_page.dart';
+import 'home.dart';
+import 'screens/home_page/my_home_page.dart';
 
 void main() {
   initializeDateFormatting().then((_) => runApp(const MyApp()));
