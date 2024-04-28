dba651eaffd5b21cf13569b3738bc9311fa44646
owner <shto43@naver.com>
2024-04-28 14:54:10 +0900

    navi

--- lib/model/storeModel.dart
+++ lib/model/storeModel.dart
@@ -1,23 +1,24 @@
 import 'dart:developer';
 
 class Store {
+  final String id;
   final String name;
   final double latitude;
   final double longitude;
   final String subscript;
 
   Store({
+    required this.id,
     required this.name,
     required this.subscript,
     required this.latitude,
     required this.longitude,
   });
 
-  factory Store.fromJson(Map<String, dynamic>? json) {
+  factory Store.fromJson(String id,Map<String, dynamic>? json) {
     if (json == null) {
       throw ArgumentError("json data is null");
     }
-
     final name = json["name"];
     final subscript = json["subscript"];
     final latitude = (json["latitude"] ?? 0).toDouble();
@@ -26,6 +27,7 @@ class Store {
     log("$name $subscript $latitude $longitude");
 
     return Store(
+      id: id,
       name: name,
       subscript: subscript,
       latitude: latitude,
