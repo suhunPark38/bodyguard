9cbcc36b5b8bbf69061de8f97bc9c4e69e289966
taemin1971355 <19taemin@hansung.ac.kr>
2024-05-07 20:50:15 +0900

    스토어 리스트 성분정보버튼 누락된 거 추가

--- lib/screens/store_list_page/store_list_page.dart
+++ lib/screens/store_list_page/store_list_page.dart
@@ -118,27 +118,8 @@ class StoreListPage extends StatelessWidget  {
                                 height: 70, // 이미지 높이
                                 fit: BoxFit.fitWidth, // 이미지 채우기 모드
                               ),
-                              IconButton(
-                                icon: const Icon(Icons.info),
-                                onPressed: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: Text('${menu.menuName} - 영양성분'),
-                                        content: Text(
-                                            '칼로리: ${menu.calories},'
-                                                ' 탄수화물: ${menu.carbohydrate},'
-                                                ' 지방: ${menu.fat},'
-                                                ' 단백질: ${menu.protein},'
-                                                ' 나트륨: ${menu.sodium},'
-                                                ' 당: ${menu.sugar}'
-                                        ),
-                                      );
-                                    },
-                                  );
-                                },
-                              ),
+                              NutrientInfoButton(
+                                  size: 20, menu: menu),
                             ],
                           ),
                           title: AutoSizeText(
