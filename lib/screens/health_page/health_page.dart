f73699421defa3bd2ee3c1e46fdefd86a4dc2ae6
taemin1971355 <19taemin@hansung.ac.kr>
2024-05-15 13:54:54 +0900

    자잘

--- lib/screens/health_page/health_page.dart
+++ lib/screens/health_page/health_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class HealthPage extends StatelessWidget {
   const HealthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
-
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("BODYGUARD"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "식단"),
+              Tab(text: "활동"),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text("식단")),
+            Center(child: Text("활동")),
+          ],
+        ),
+      ),
     );
   }
 }
