4afa665bc03023d43ce942ac00971ac9d24e1158
taemin1971355 <19taemin@hansung.ac.kr>
2024-05-29 18:58:01 +0900

    홈페이지 광고 개선

--- lib/screens/home_page/widget/ad_carousel.dart
+++ lib/screens/home_page/widget/ad_carousel.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:bodyguard/model/ad_model.dart';
-import 'package:bodyguard/services/ad_service.dart';
-import 'package:bodyguard/services/store_service.dart';
-import 'package:bodyguard/screens/store_menu_page/store_menu_page.dart';
-
-import '../../../model/store_model.dart';
+import 'package:provider/provider.dart';
+import 'package:bodyguard/providers/ad_provider.dart';
 
 class AdCarousel extends StatelessWidget {
-  final AdService adService = AdService();
-  final StoreService storeService = StoreService();
-
-  AdCarousel({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Ad>>(
-      future: adService.fetchAds(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+    return Consumer<AdProvider>(
+      builder: (context, adProvider, _) {
+        if (adProvider.ads.isEmpty) {
+          adProvider.fetchAds(); // 데이터 가져오기
           return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return const Center(child: Text('광고를 불러오는 중 오류가 발생했습니다.'));
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('광고가 없습니다.'));
         } else {
-          final ads = snapshot.data!;
+          final ads = adProvider.ads;
           return CarouselSlider(
             options: CarouselOptions(
               height: 210,
@@ -41,19 +28,7 @@ class AdCarousel extends StatelessWidget {
                 builder: (BuildContext context) {
                   return GestureDetector(
                     onTap: () async {
-                      Store? store =
-                          await storeService.getStoreByName(ad.storeName);
-                      if (store != null) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => StoreMenuPage(store: store),
-                          ),
-                        );
-                      } else {
-                        // store가 null인 경우에 대한 처리 (예: 에러 메시지 표시)
-                        print('해당 가게를 찾을 수 없습니다.');
-                      }
+                      // onTap 이벤트 처리
                     },
                     child: Card(
                       shape: RoundedRectangleBorder(
