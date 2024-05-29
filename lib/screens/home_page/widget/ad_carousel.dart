5041134e071629123680326d4e391169eef9a318
taemin1971355 <19taemin@hansung.ac.kr>
2024-05-29 19:04:46 +0900

    홈페이지 광고 개선

--- lib/screens/home_page/widget/ad_carousel.dart
+++ lib/screens/home_page/widget/ad_carousel.dart
@@ -3,7 +3,13 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:provider/provider.dart';
 import 'package:bodyguard/providers/ad_provider.dart';
 
+import '../../../model/store_model.dart';
+import '../../../services/store_service.dart';
+import '../../store_menu_page/store_menu_page.dart';
+
 class AdCarousel extends StatelessWidget {
+  final StoreService storeService = StoreService();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AdProvider>(
@@ -28,7 +34,19 @@ class AdCarousel extends StatelessWidget {
                 builder: (BuildContext context) {
                   return GestureDetector(
                     onTap: () async {
-                      // onTap 이벤트 처리
+                      Store? store =
+                      await storeService.getStoreByName(ad.storeName);
+                      if (store != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => StoreMenuPage(store: store),
+                          ),
+                        );
+                      } else {
+                        // store가 null인 경우에 대한 처리 (예: 에러 메시지 표시)
+                        print('해당 가게를 찾을 수 없습니다.');
+                      }
                     },
                     child: Card(
                       shape: RoundedRectangleBorder(
