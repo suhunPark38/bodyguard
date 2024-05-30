023a07a0536d61c7857dc703f02ebf970a7f4e13
owner <shto43@naver.com>
2024-05-30 14:03:21 +0900

    uifixz

--- lib/screens/shopping_page/shopping_page.dart
+++ lib/screens/shopping_page/shopping_page.dart
@@ -105,25 +105,23 @@ class ShoppingPage extends StatelessWidget {
                                           width: 90,
                                           height: 20,
                                           child: CustomButton(
-                                            onPressed: provider.selectedMenus.isNotEmpty
-                                                ? () {
-                                              provider.completePayment(context);
-                                              provider.refreshPayments();
-                                              provider.handleReset(); // 결제를 완료 후 장바구니 데이터 클리어
-                                              provider.setCurrentShoppingTabIndex(1);
-                                              Navigator.pushAndRemoveUntil(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) => const MyHomePage(
-                                                      initialIndex: 3,
-                                                    )),
-                                                    (route) => false,
-                                              );
-                                            }
                                             text: const Text(
                                               '메뉴 담기',
                                               style: TextStyle(fontSize: 10),
                                             ),
+                                            onPressed: () async {
+                                              provider.completePayment(context);
+                                              provider.refreshPayments();
+                                              provider.handleReset(); // 결제를 완료 후 장바구니 데이터 클리어
+                                              provider.setCurrentStoreTabIndex(0);
+                                              provider.setCurrentStoreTabIndex(0);
+                                              await Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) => StoreListPage(),
+                                                ),
+                                              );
+                                            },
                                           ),
                                         ),
                                       ],
